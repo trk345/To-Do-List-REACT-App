24afd1cc749358edf993cb87932f0546dbc4b579
trk345 <tawhidkhan3@gmail.com>
2025-01-19 22:32:28 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/trk345.github.io/',
+  base: '/To-Do-List-REACT-App/',
   plugins: [react()],
 })
